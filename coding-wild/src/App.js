e5ff742bcda73703694f832bb98cd964d967557d
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-03-10 23:57:43 -0600

    Implement Flowbox Information Section (temporary).

--- coding-wild/src/App.js
+++ coding-wild/src/App.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 // Components
 import Header from './components/Global/Header';
 import Footer from './components/Global/Footer';
-import Landing from './components/Landing/'
+import Landing from './components/Landing/';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            DON'T Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
         <Landing/>
         <Footer/>
       </div>
--- coding-wild/src/components/Landing/index.js
+++ coding-wild/src/components/Landing/index.js
@@ -7,20 +7,24 @@ class Landing extends Component {
         <section className="hero-banner">
           <div className="wrapper">
             <h2>
-              Code like you've never coded before.
+              Code like you've never coded before. <br/> Code wild.
             </h2>
           </div>
         </section>
 
         <section className="work-flow">
-          <div className="flowbox">
-            Prepare
-          </div>
-          <div className="flowbox">
-            Embark
-          </div>
-          <div className="flowbox">
-            Code
+          <div className="wrapper">
+            <div className="flowbox">
+              Prepare
+            </div>
+            <p className="arrow">⇒</p>
+            <div className="flowbox">
+              Embark
+            </div>
+            <p className="arrow">⇒</p>
+            <div className="flowbox">
+              Code
+            </div>
           </div>
         </section>
 
